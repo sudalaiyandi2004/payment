5da8faa2540b9a54ac79cfd43e289059648876dc
sudalaiyandi sabarish kumar <sudalaiyandi@student.tce.edu>
2024-11-30 12:19:11 +0530

    Updated app with new changes

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ function App() {
       <div className="App">
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/payment">Home</Link></li>
+            <li><Link to="/payment/about">About</Link></li>
+            <li><Link to="/payment/contact">Contact</Link></li>
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/payment" element={<Home />} />
+          <Route path="/payment/about" element={<About />} />
+          <Route path="/payment/contact" element={<Contact />} />
         </Routes>
       </div>
     </BrowserRouter>
